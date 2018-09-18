e99aec94f0b9843a5128cc131a1e69b63ea60aba
Александр Чаусов <traktoro_13@mail.ru>
2018-09-18 22:04:29 +0300

    added xcode project

---
+++ FlatPageControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlatPageControl
+//
+//  Created by Александр Чаусов on 18.09.2018.
+//  Copyright © 2018 surf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FlatPageControlTests/FlatPageControlTests.swift
@@ -0,0 +1,36 @@
+//
+//  FlatPageControlTests.swift
+//  FlatPageControlTests
+//
+//  Created by Александр Чаусов on 18.09.2018.
+//  Copyright © 2018 surf. All rights reserved.
+//
+
+import XCTest
+@testable import FlatPageControl
+
+class FlatPageControlTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
