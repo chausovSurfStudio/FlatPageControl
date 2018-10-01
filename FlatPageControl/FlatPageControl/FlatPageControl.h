d66e8fdba7c5d240f2cd34d6f785eeffc416a43e
Александр Чаусов <traktoro_13@mail.ru>
2018-10-01 23:43:01 +0300

    init new project for cocoa touch framework

---
+++ FlatPageControl/FlatPageControl/FlatPageControl.h
@@ -0,0 +1,19 @@
+//
+//  FlatPageControl.h
+//  FlatPageControl
+//
+//  Created by Александр Чаусов on 01.10.2018.
+//  Copyright © 2018 Surf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlatPageControl.
+FOUNDATION_EXPORT double FlatPageControlVersionNumber;
+
+//! Project version string for FlatPageControl.
+FOUNDATION_EXPORT const unsigned char FlatPageControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlatPageControl/PublicHeader.h>
+
+
