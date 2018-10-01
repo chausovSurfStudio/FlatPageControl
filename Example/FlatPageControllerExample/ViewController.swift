a8816157af0e64d8d1b8f3aa9b857bf3cc152865
Александр Чаусов <traktoro_13@mail.ru>
2018-10-02 00:14:01 +0300

    init example project

---
+++ Example/FlatPageControllerExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlatPageControllerExample
+//
+//  Created by Александр Чаусов on 02.10.2018.
+//  Copyright © 2018 Surf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Source/FlatPageControl.swift
@@ -0,0 +1,11 @@
+//
+//  FlatPageControl.swift
+//  FlatPageControl
+//
+//  Created by Александр Чаусов on 02.10.2018.
+//  Copyright © 2018 Surf. All rights reserved.
+//
+
+final class FlatPageControl: UIPageControl {
+    
+}
