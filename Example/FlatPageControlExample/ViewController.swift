f9710de174f4d6f7f6c4d7098fa4742d25aafafd
Александр Чаусов <traktoro_13@mail.ru>
2018-10-05 22:30:37 +0300

    add button for test, remove unused code

--- Example/FlatPageControlExample/ViewController.swift
+++ Example/FlatPageControlExample/ViewController.swift
@@ -11,11 +11,36 @@ import FlatPageControl
 
 class ViewController: UIViewController {
     
+    // MARK: - IBOutlets
+    
+    @IBOutlet private weak var pageControl: FlatPageControl!
+    
+    // MARK: - Private Properties
+    
+    private var currentPage: Int = 0
+    
+    // MARK: - UIViewController
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        pageControl.numberOfPages = 25
+        pageControl.setCurrentPage(currentPage, animated: false)
     }
-
 
 }
 
+// MARK: - Actions
+
+private extension ViewController {
+    
+    @IBAction func tapOnPrevButton(_ sender: UIButton) {
+        currentPage -= 1
+        pageControl.setCurrentPage(currentPage, animated: true)
+    }
+    
+    @IBAction func tapOnNextButton(_ sender: UIButton) {
+        currentPage += 1
+        pageControl.setCurrentPage(currentPage, animated: true)
+    }
+    
+}
