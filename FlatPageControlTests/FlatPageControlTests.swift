b141fd2d6d98bce9c3c06e1603b308200e8dfe60
Александр Чаусов <traktoro_13@mail.ru>
2018-10-11 23:30:04 +0300

    add tests on flat page control

--- FlatPageControlTests/FlatPageControlTests.swift
+++ FlatPageControlTests/FlatPageControlTests.swift
@@ -11,12 +11,79 @@ import XCTest
 
 class FlatPageControlTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testFlatPageControl_1() {
+        let pageControl = FlatPageControl(frame: .zero)
+        XCTAssertNotNil(pageControl.view)
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testFlatPageControl_2() {
+        let pageControl = FlatPageControl(frame: .zero)
+        pageControl.numberOfPages = 10
+        pageControl.setCurrentPage(5, animated: false)
+        XCTAssertEqual(pageControl.currentPage, 5)
+        pageControl.setCurrentPage(15, animated: false)
+        XCTAssertEqual(pageControl.currentPage, 5)
+    }
+
+    func testFlatPageControl_3() {
+        let pageControl = FlatPageControl(frame: .zero)
+        pageControl.numberOfPages = 10
+        pageControl.setCurrentPage(5, animated: true)
+        XCTAssertEqual(pageControl.currentPage, 5)
+        pageControl.setCurrentPage(15, animated: true)
+        XCTAssertEqual(pageControl.currentPage, 5)
+    }
+
+    func testFlatPageControl_4() {
+        let pageControl = FlatPageControl(frame: .zero)
+        pageControl.numberOfPages = 10
+        pageControl.setCurrentPage(5, animated: true)
+        XCTAssertEqual(pageControl.currentPage, 5)
+        pageControl.setCurrentPage(6, animated: true)
+        XCTAssertEqual(pageControl.currentPage, 6)
+    }
+
+    func testFlatPageControl_5() {
+        let pageControl = FlatPageControl(frame: .zero)
+        pageControl.hidesForSinglePage = true
+        pageControl.numberOfPages = 1
+        XCTAssertEqual(pageControl.containerView.isHidden, true)
+    }
+
+    func testFlatPageControl_6() {
+        let pageControl = FlatPageControl(frame: .zero)
+        pageControl.numberOfPages = 20
+        pageControl.maxPagesNumber = 10
+        pageControl.setCurrentPage(5, animated: true)
+        XCTAssertEqual(pageControl.currentPage, 5)
+        pageControl.setCurrentPage(6, animated: true)
+        XCTAssertEqual(pageControl.currentPage, 6)
+        pageControl.setCurrentPage(5, animated: true)
+        XCTAssertEqual(pageControl.currentPage, 5)
+    }
+
+    func testFlatPageControl_7() {
+        let pageControl = FlatPageControl(frame: .zero)
+        pageControl.numberOfPages = 20
+        pageControl.maxPagesNumber = 10
+        pageControl.setCurrentPage(19, animated: true)
+        XCTAssertEqual(pageControl.currentPage, 19)
+        pageControl.setCurrentPage(0, animated: true)
+        XCTAssertEqual(pageControl.currentPage, 0)
+        pageControl.setCurrentPage(1, animated: true)
+        XCTAssertEqual(pageControl.currentPage, 1)
+    }
+
+    func testFlatPageControl_8() {
+        let pageControl = FlatPageControl(frame: .zero)
+        pageControl.numberOfPages = 20
+        pageControl.maxPagesNumber = 10
+        pageControl.setCurrentPage(0, animated: true)
+        XCTAssertEqual(pageControl.currentPage, 0)
+        pageControl.setCurrentPage(19, animated: true)
+        XCTAssertEqual(pageControl.currentPage, 19)
+        pageControl.setCurrentPage(19, animated: true)
+        XCTAssertEqual(pageControl.currentPage, 19)
     }
 
 }
