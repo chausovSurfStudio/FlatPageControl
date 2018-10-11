c1e916249dc4abc9481139c68470e5d4f5fd2b31
Александр Чаусов <traktoro_13@mail.ru>
2018-10-11 23:05:00 +0300

    add tests on page indicator view

---
+++ FlatPageControlTests/SupportClassesTest.swift
@@ -0,0 +1,33 @@
+//
+//  SuppertClassesTest.swift
+//  FlatPageControlTests
+//
+//  Created by Александр Чаусов on 11.10.2018.
+//  Copyright © 2018 Surf. All rights reserved.
+//
+
+import XCTest
+@testable import FlatPageControl
+
+class SupportClassesTest: XCTestCase {
+
+    func testPageIndicator_1() {
+        let pageIndicator = FlatPageIndicator(frame: .zero, tintColor: .red)
+        for subview in pageIndicator.subviews {
+            XCTAssertEqual(subview.backgroundColor, .red)
+        }
+        pageIndicator.changeIndicatorColor(.green)
+        for subview in pageIndicator.subviews {
+            XCTAssertEqual(subview.backgroundColor, .green)
+        }
+    }
+
+    func testPageIndicator_2() {
+        let pageIndicator = FlatPageIndicator(frame: .zero)
+        pageIndicator.changeIndicatorColor(.purple)
+        for subview in pageIndicator.subviews {
+            XCTAssertEqual(subview.backgroundColor, .purple)
+        }
+    }
+
+}
